9dc6bbdc10aff0bd5e39444456715ade07020332
DrSecant <pellegrino.software.solutions@gmail.com>
2023-01-27 19:21:29 -0500

    Initial file commit, reorganization of file structure

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+const DURATION = 500;
+const KEY_SCALES = [2, 5, 15, 2, 10, 12, 4];
+let tl = anime.timeline({
+  direction: 'alternate',
+  duration: DURATION,
+  loop: true,
+  easing: 'linear'
+});
+
+KEY_SCALES.forEach((scale, seed) => {
+  tl.add({
+    targets: ['.disp', '.turb'],
+    scale: scale,
+    seed: seed,
+    duration: DURATION / KEY_SCALES.length
+  });
+});
