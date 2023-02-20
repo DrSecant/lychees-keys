ee1953d5850fae4b790fbf5d2e2072eec600ae3b
DrSecant <pellegrino.software.solutions@gmail.com>
2023-02-20 17:55:57 -0500

    added section with contact info

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,9 @@
-const { Grid, Item, Box, Card, CardContent, Typography, Divider, Slide, Avatar } = window['MaterialUI'];
+const { Grid, Item, Box, Card, CardContent, Typography, Divider, Slide, Avatar, List, ListItem, ListItemAvatar, ListItemText, IconButton } = window['MaterialUI'];
 
 const Contact = () => (
   <div class="contact-wrapper">
     <Grid container direction="column">
+      {/* About Card */}
       <Grid display="flex" justifyContent="center" alignItems="center">
         <Box sx={{ width: '40%' }}>
           <Slide direction="left" in={true} mountOnEnter unmountOnExit>
@@ -30,6 +31,80 @@ const Contact = () => (
                   Tapping into the creative skills he's built his whole life he now crafts work that blossoms from the intricate 
                   weave of character and plot.
                   </Typography>
+                </Box>
+              </CardContent>
+            </Card>
+          </Slide>
+        </Box>
+      </Grid>
+
+      {/* Contact Card */}
+      <Grid display="flex" justifyContent="center" alignItems="center">
+        <Box sx={{ width: '40%' }}>
+          <Slide direction="left" in={true} mountOnEnter unmountOnExit>
+            <Card id="contactCard" className="info-card">
+              <CardContent>
+                <Typography className="card-title" sx={{ ml: 2, mb: 2 }} variant="h5" component="div">
+                  Where to find me
+                </Typography>
+                <Divider className="card-divider" variant="middle" />
+                <Box className="card-content" sx={{ m: 2, mb: 0 }}>
+                  <Typography sx={{ mb: 1 }} variant="body1">
+                  You can find my work or reach out to me in the following ways:
+                  </Typography>
+                  <List className="social-info">
+                    {/* Vimeo Link */}
+                    <ListItem
+                      secondaryAction={
+                        <IconButton size="small" aria-label="copy">
+                          <i className="fa-regular fa-copy"></i>
+                        </IconButton>
+                      }
+                    >
+                      <ListItemAvatar>
+                        <Avatar className="social-icon">
+                          <i className="fa-brands fa-vimeo-v fa-sm"></i>
+                        </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText className="social-text"
+                        primary="My Vimeo Page"
+                      />
+                    </ListItem>
+                    {/* Instagram Link */}
+                    <ListItem
+                      secondaryAction={
+                        <IconButton size="small" aria-label="copy">
+                          <i className="fa-regular fa-copy"></i>
+                        </IconButton>
+                      }
+                    >
+                      <ListItemAvatar>
+                        <Avatar className="social-icon">
+                          <i className="fa-brands fa-instagram fa-sm"></i>
+                        </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText className="social-text"
+                        primary="@lychees.keys"
+                      />
+                    </ListItem>
+                    {/* Email Link */}
+                    <ListItem
+                      secondaryAction={
+                        <IconButton size="small" aria-label="copy">
+                          <i className="fa-regular fa-copy"></i>
+                        </IconButton>
+                      }
+                    >
+                      <ListItemAvatar>
+                        <Avatar className="social-icon">
+                          <i className="fa-solid fa-envelope fa-sm"></i>
+                        </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText className="social-text"
+                        primary="lychees.keys@gmail.com"
+                      />
+                    </ListItem>
+                  </List>
                 </Box>
               </CardContent>
             </Card>
